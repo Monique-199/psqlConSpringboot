4d0ccb278cdf12ed6c58ec5b01f017c9ef0f9b86
Monicah Kerubo <106078042+Monique-199@users.noreply.github.com>
2024-09-04 07:16:22 +0300

    Basic postgres connection

---
+++ src/main/java/com/mkerubo/psqlConnection/PsqlConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.mkerubo.psqlConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsqlConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsqlConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkerubo/psqlConnection/PsqlConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkerubo.psqlConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsqlConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
